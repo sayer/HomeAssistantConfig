e469056da2edba2eff5f46502b536d2ebe89f2f0
Stephen Ayers <sayer@ayersnet.com>
2025-04-23 16:39:08 -0600

    logging

--- docker_shell.sh
+++ docker_shell.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "whoami: `whoami`"
+echo "Boring into docker container rvc2mqtt"
 sudo docker exec -it $(sudo docker ps --filter "name=rvc2" --format "{{.Names}}" | head -n 1) bash
+echo "Exiting docker container rvc2mqtt"
