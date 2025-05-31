0eb077c3dd60006d26a71fb61dcdb853e19cbdf9
Stephen Ayers <sayer@ayersnet.com>
2025-05-30 21:39:18 -0600

    MY 2017 use ramp toggle

--- update_config.sh
+++ update_config.sh
@@ -593,8 +593,8 @@ configure_2017_to_2019() {
     OPTIMISTIC_MODE="false"
     AMBIANT_TEMP="249"
     INDOOR_TEMP="250"
-    LIGHT_COMMAND_ON="0"
-    LIGHT_COMMAND_OFF="3"
+    LIGHT_COMMAND_ON="18"
+    LIGHT_COMMAND_OFF="18"
     # Set shade codes
     set_shade_tokens "CODE_2017"
 }
